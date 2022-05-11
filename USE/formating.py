b994de9c598b9097630d877476b4b744a1571d41
Kim-EunMin <58218596+Kim-EunMin@users.noreply.github.com>
2022-05-11 10:53:31 +0900

    Update formating.py

--- USE/formating.py
+++ USE/formating.py
@@ -23,3 +23,16 @@ coffee_val = coffee1_val + coffee2_val + coffee3_val
 print('손님, \n%s, %s, %s를 주문하셨습니다.' % (coffee1_name, coffee2_name, coffee3_name))
 print('손님, \n %d 총 가격 입니다'%(coffee_val))
 
+
+
+------------------------------------------------------------------------------------------------
+for i in range(1,10,2):
+    mark =  '{space}{mark}  {space} i = {idx}'.format(                                            #--> {} 안에 space, mark 라고 넣어두면 이해하기편함
+                space = ' ' * int((10-i)/2),  # int 니까 소수점 이하는 다 날라간다.
+                mark  = '*' * i,
+                idx   = i                      
+                )
+    print(mark)
+
+    
+    
