4c21f21cea1b9aae5831478fd63004d59cb231a0
Kim-EunMin <58218596+Kim-EunMin@users.noreply.github.com>
2022-05-23 10:53:13 +0900

    Create 14.효율적인주석문처리.py

---
+++ USE/14.효율적인주석문처리.py
@@ -0,0 +1,22 @@
+#파이썬 마크다운 효율적으로 사용하기 (ctrl +m)
+
+#예제 (전부 ctrl+m 눌러주기)
+
+
+# Python Review : 변수, 데이터타입, 제어문 
+
+### <font color = 'brown'> Jupyter Notebook 소개 </font>
+> Introduction & Tips
+- Jupyter Notebook : Interaction Python Programming Tool
+- Tab : 자동완성 기능 어느 정도 가능             #4줄이 한묶음
+
+  
+### <font color = 'brown'> 1.변수 </font>
+
+### <font color = 'brown'> 2. 데이터타입 </font>
+
+* 숫자형, Numeric Type
+
+* 논리형, Boolean Type
+
+### <font color = 'brown'> 3. 조건문 </font>
